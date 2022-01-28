428f103ffc742fa04a9d8276c5a9be8e78068982
Vladislav Lunov <vladllunov1212@gmail.com>
2022-01-28 23:00:15 +0200

    upload.py script

---
+++ upload.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, send_file
+
+app = Flask(__name__)
+
+ip = "null"
+file_path = "null"
+
+@app.route("/", methods=["GET"])
+def main():
+    if (request.remote_addr == ip):
+        return send_file(file_path)#, attachment_filename='python.jpg')
+    else:
+        return "Access denied"
+
+@app.route("/ip", methods=["GET"])
+def ip():
+    return "<h1>"+request.remote_addr+"</h1>"
+
+app.run(host="0.0.0.0")
